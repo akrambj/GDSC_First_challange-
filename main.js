500e801cf62e33717cf2bb90d99577138e02c751
akrambj <bedjaouiakram.93@gmail.com>
2022-11-13 09:22:36 +0000

    js

---
+++ main.js
@@ -0,0 +1,7 @@
+        const toggle = document.getElementById('toggle');
+        const navbar = document.getElementById('navbar');
+        
+        toggle.onclick = function(){
+            toggle.classList.toggle('active')
+            navbar.classList.toggle('active')
+        }
